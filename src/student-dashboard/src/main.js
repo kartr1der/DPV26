1d3a77063e410df2144e409cc6df366814133f39
kartr1der <147752381+kartr1der@users.noreply.github.com>
2026-06-14 19:58:33 +0500

    Update main.js

--- src/student-dashboard/src/main.js
+++ src/student-dashboard/src/main.js
@@ -1,4 +1,10 @@
 import { createApp } from 'vue'
-import App from './app/App.vue'
+import { createPinia } from 'pinia'
+import App from './App.vue'
+import router from './router'
+import './app/styles/global.css'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.use(createPinia())
+app.use(router)
+app.mount('#app')
