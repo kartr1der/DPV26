00318093a61545853d00d58391b35aa9509e6149
kartr1der <147752381+kartr1der@users.noreply.github.com>
2026-06-14 19:59:44 +0500

    Add files via upload

---
+++ src/student-dashboard/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+<router-view />
+</template>
+<script>
+import { useUserStore } from '@/stores/user'
+
+export default {
+  name: 'App',
+  created() {
+    const userStore = useUserStore()
+    userStore.restoreSession()
+  }
+}
+</script><style>
+@import 'bootstrap/dist/css/bootstrap.min.css';
+@import 'bootstrap-icons/font/bootstrap-icons.css';
+/* остальные глобальные стили уже подключены в main.js или через @import */
+</style>
